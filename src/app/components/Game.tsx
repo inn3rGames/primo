cc31a0f3a3b6534e6b004f70f27e783e23f96e9f
inn3rGames <hi@inn3r.com>
2024-08-13 18:24:27 +0300

    Create main container

--- src/app/components/Game.tsx
+++ src/app/components/Game.tsx
@@ -1,4 +1,4 @@
-import { Container, Sprite, Application, Texture } from "pixi.js";
+import { Container, Sprite, Application, Texture, Assets } from "pixi.js";
 import scaleToFit from "../utils/scaleToFit";
 
 // Handle main game logic
@@ -7,13 +7,35 @@ const Game = (app: Application) => {
   const gameWidth = 800;
   const gameHeight = 600;
   const backgroundColor = 0xffff00;
-
   app.renderer.background.color = backgroundColor;
 
-  app.ticker.add((delta) => {
+  // Create main container
+  const mainContainer = new Container();
+  app.stage.addChild(mainContainer);
+
+  // Load texture
+  const texture = Texture.from("https://pixijs.com/assets/bunny.png");
+
+  // Create a 5x5 grid sprites grid
+  for (let i = 0; i < 25; i++) {
+    const sprite = new Sprite(texture);
+
+    sprite.x = (i % 5) * 40;
+    sprite.y = Math.floor(i / 5) * 40;
+    mainContainer.addChild(sprite);
+  }
+
+  // Center main container
+  mainContainer.x = app.screen.width / 2;
+  mainContainer.y = app.screen.height / 2;
+  mainContainer.pivot.x = mainContainer.width / 2;
+  mainContainer.pivot.y = mainContainer.height / 2;
+
+  app.ticker.add(() => {
     // Handle resize
     const currentScale = scaleToFit(gameWidth, gameHeight);
     app.renderer.resize(gameWidth * currentScale, gameHeight * currentScale);
+    app.stage.scale.set(currentScale);
   });
 };
 
