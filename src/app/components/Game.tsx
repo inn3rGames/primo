d4d1714363b97979e539309930328467aee278c2
inn3rGames <hi@inn3r.com>
2024-08-13 21:56:12 +0300

    Add basic update loop

--- src/app/components/Game.tsx
+++ src/app/components/Game.tsx
@@ -1,9 +1,9 @@
-import { Container, Application, Text } from "pixi.js";
+import { Container, Application, Text, TextStyleLineJoin } from "pixi.js";
 import scaleToFit from "../utils/scaleToFit";
 
 // Handle main game logic
 const Game = async (app: Application) => {
-  //Default stage options
+  // Default stage options
   const gameWidth = 800;
   const gameHeight = 600;
   const backgroundColor = "#ADD8E6";
@@ -14,20 +14,39 @@ const Game = async (app: Application) => {
   const mainContainer = new Container();
   app.stage.addChild(mainContainer);
 
-  const text1 = new Text();
-  text1.text = "1";
-  text1.style.fill = ["#FFD700", "#FFA500"];
-  text1.style.fontFamily = "Casino 3D Filled Marquee";
-  text1.style.fontSize = 100;
-  text1.style.stroke = "#FFFF00";
-  text1.style.strokeThickness = 3;
-  text1.style.lineJoin = "round";
-  text1.style.dropShadow = true;
-  text1.style.dropShadowColor = "#000000";
-  text1.style.dropShadowAlpha = 0.25;
-  text1.style.dropShadowBlur = 3;
+  // Custom text style
+  const customStyle = {
+    fill: ["#FFD700", "#FFA500"],
+    fontFamily: "Casino 3D Filled Marquee",
+    fontSize: 100,
+    stroke: "#FFFF00",
+    strokeThickness: 3,
+    lineJoin: "round" as TextStyleLineJoin,
+    dropShadow: true,
+    dropShadowColor: "#000000",
+    dropShadowAlpha: 0.25,
+    dropShadowBlur: 3,
+  };
 
-  mainContainer.addChild(text1);
+  // Create numbers container
+  const numbersContainer = new Container();
+  app.stage.addChild(numbersContainer);
+
+  // Hold all numbers
+  let numbers: Array<Text> = [];
+
+  const createNumbers = (totalNumbers: number) => {
+    for (let i = 0; i < totalNumbers + 1; i++) {
+      const number = new Text();
+      number.text = i.toString();
+      number.style = { ...customStyle };
+      number.y = -105 * i + gameHeight;
+      numbersContainer.addChild(number);
+      numbers.push(number);
+    }
+  };
+
+  createNumbers(20);
 
   // Center main container
   mainContainer.x = app.screen.width / 2;
@@ -35,8 +54,14 @@ const Game = async (app: Application) => {
   mainContainer.pivot.x = mainContainer.width / 2;
   mainContainer.pivot.y = mainContainer.height / 2;
 
+  // Center numbers container
+  numbersContainer.x = app.screen.width / 2;
+  numbersContainer.y = app.screen.height / 2;
+  numbersContainer.pivot.x = numbersContainer.width / 2;
+  numbersContainer.pivot.y = numbersContainer.height / 2;
+
+  // Handle resize
   app.ticker.add(() => {
-    // Handle resize
     const currentScale = scaleToFit(
       window.innerWidth,
       window.innerHeight,
@@ -45,8 +70,14 @@ const Game = async (app: Application) => {
     );
     app.renderer.resize(gameWidth * currentScale, gameHeight * currentScale);
     app.stage.scale.set(currentScale);
+  });
 
-    text1.updateText(false);
+  // Main game update loop
+  app.ticker.add((delta) => {
+    numbersContainer.y += delta * 10;
+    if (numbersContainer.y >= numbersContainer.height + gameHeight * 2) {
+      numbersContainer.y = 0;
+    }
   });
 };
 
