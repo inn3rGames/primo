2d3bdfc0c110c2f958bfcd1a2424b0e4b7d0e82a
inn3rGames <hi@inn3r.com>
2024-08-13 20:46:57 +0300

    Improve font style

--- src/app/components/Game.tsx
+++ src/app/components/Game.tsx
@@ -1,4 +1,4 @@
-import { Container, Sprite, Application, Texture, Assets, Text } from "pixi.js";
+import { Container, Application, Text } from "pixi.js";
 import scaleToFit from "../utils/scaleToFit";
 
 // Handle main game logic
@@ -6,31 +6,26 @@ const Game = async (app: Application) => {
   //Default stage options
   const gameWidth = 800;
   const gameHeight = 600;
-  const backgroundColor = 0xffff00;
+  const backgroundColor = "#ADD8E6";
   app.renderer.background.color = backgroundColor;
-  app.renderer.background.alpha = 0.9;
+  app.renderer.background.alpha = 0.75;
 
   // Create main container
   const mainContainer = new Container();
   app.stage.addChild(mainContainer);
 
-  // Load texture
-  const texture = Texture.from("https://pixijs.com/assets/bunny.png");
-
-  // Create a 5x5 grid sprites grid
-  for (let i = 0; i < 25; i++) {
-    const sprite = new Sprite(texture);
-
-    sprite.x = (i % 5) * 40;
-    sprite.y = Math.floor(i / 5) * 40;
-    mainContainer.addChild(sprite);
-  }
-
   const text1 = new Text();
-  text1.text = "1234567890";
-  text1.style.fontFamily = "Casino 3D Lines Marquee";
+  text1.text = "1";
+  text1.style.fill = ["#FFD700", "#FFA500"];
+  text1.style.fontFamily = "Casino 3D Filled Marquee";
   text1.style.fontSize = 100;
-  text1.y = 200;
+  text1.style.stroke = "#FFFF00";
+  text1.style.strokeThickness = 3;
+  text1.style.lineJoin = "round";
+  text1.style.dropShadow = true;
+  text1.style.dropShadowColor = "#000000";
+  text1.style.dropShadowAlpha = 0.25;
+  text1.style.dropShadowBlur = 3;
 
   mainContainer.addChild(text1);
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,11 @@ const NoSSRContainer = dynamic(() => import("./components/NoSSRContainer"));
 // Default Game Page
 export default function Home() {
   return (
-    <div id="no-ssr-container">
-      <NoSSRContainer></NoSSRContainer>
+    <div>
+      <div id="blurred-background"></div>
+      <div id="no-ssr-container">
+        <NoSSRContainer></NoSSRContainer>
+      </div>
     </div>
   );
 }
