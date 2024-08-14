6fc762e4b5ab45bcf1c26d7ce047bf619451c000
inn3rGames <hi@inn3r.com>
2024-08-14 11:23:43 +0300

    Create game background

--- src/app/components/Game.tsx
+++ src/app/components/Game.tsx
@@ -4,6 +4,7 @@ import {
   Text,
   TextStyleLineJoin,
   TextStyleAlign,
+  Graphics,
 } from "pixi.js";
 import scaleToFit from "../utils/scaleToFit";
 
@@ -14,7 +15,7 @@ const Game = async (app: Application) => {
   const gameHeight = 600;
   const backgroundColor = "#ADD8E6";
   app.renderer.background.color = backgroundColor;
-  app.renderer.background.alpha = 0.75;
+  app.renderer.background.alpha = 0;
 
   // Create main container
   const mainContainer = new Container();
@@ -43,18 +44,19 @@ const Game = async (app: Application) => {
   let numbers: Array<Text> = [];
 
   const createNumbers = (totalNumbers: number) => {
-    for (let i = 0; i < totalNumbers + 1; i++) {
+    for (let i = 1; i < totalNumbers + 1; i++) {
       const number = new Text();
       number.text = i.toString();
       number.style = { ...customStyle };
-      number.y = -105 * i + gameHeight;
-      number.anchor.set(0.5, 0.5);
+      number.x = 0;
+      number.y = -6;
+      console.log(number.height);
       numbersContainer.addChild(number);
       numbers.push(number);
     }
   };
 
-  createNumbers(20);
+  createNumbers(1);
 
   // Center main container
   mainContainer.x = app.screen.width / 2;
@@ -65,9 +67,33 @@ const Game = async (app: Application) => {
   // Center numbers container
   numbersContainer.x = app.screen.width / 2;
   numbersContainer.y = app.screen.height / 2;
-  numbersContainer.pivot.x =
-    numbersContainer.width / 2 - (numbers[numbers.length - 1].width / 4);
+  numbersContainer.pivot.x = numbersContainer.width / 2;
   numbersContainer.pivot.y = numbersContainer.height / 2;
+
+  // Create UI container
+  const uiContainer = new Container();
+  app.stage.addChild(uiContainer);
+
+  // Center UI container
+  uiContainer.x = app.screen.width / 2;
+  uiContainer.y = app.screen.height / 2;
+  uiContainer.pivot.x = uiContainer.width / 2;
+  uiContainer.pivot.y = uiContainer.height / 2;
+
+  // Create background rectangle
+  const backgroundRectangle = new Graphics();
+  backgroundRectangle.lineStyle(gameHeight, "#ADD8E6", 0.75, 1);
+  backgroundRectangle.drawRoundedRect(-75, -75, 150, 150, 0);
+  backgroundRectangle.endFill();
+  uiContainer.addChild(backgroundRectangle);
+
+  // Create center square
+  const square = new Graphics();
+  square.lineStyle(10, "#ADD8E6", 1, 0.5);
+  square.drawRoundedRect(-75, -75, 150, 150, 5);
+  square.endFill();
+  uiContainer.addChild(square);
+
 
   // Handle resize
   app.ticker.add(() => {
@@ -83,10 +109,10 @@ const Game = async (app: Application) => {
 
   // Main game update loop
   app.ticker.add((delta) => {
-    numbersContainer.y += delta * 10;
+    /* numbersContainer.y += delta * 10;
     if (numbersContainer.y >= numbersContainer.height + gameHeight * 2) {
       numbersContainer.y = 0;
-    }
+    } */
   });
 };
 
